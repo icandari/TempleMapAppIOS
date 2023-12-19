b0f3151151568959ff38584d4414eeeb3dc25e19
icandari <isaaccandari@gmail.com>
2023-12-18 14:44:41 -1000

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Isaac Candari on 12/18/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MapAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
